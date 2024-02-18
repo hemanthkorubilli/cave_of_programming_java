21a9046c9aa0ea9f502c2443f874bd1fc42f2d3b
hemanthkorubilli <130943446+hemanthkorubilli@users.noreply.github.com>
2024-02-18 08:35:51 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,10 @@
+class variables{
+    public static void main(String args[]){
+        int a=1;
+        char ch='a';
+        boolean bool=true;
+        System.out.println(a);
+        System.out.println(ch);
+        System.out.println(bool);
+    }
+}
