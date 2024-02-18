f42721dedcee7281bbd9e787a407f430625df8ce
hemanthkorubilli <130943446+hemanthkorubilli@users.noreply.github.com>
2024-02-18 12:27:45 +0530

    Add files via upload

---
+++ stringarrays.java
@@ -0,0 +1,8 @@
+class stringarrays{
+    public static void main(String args[]){
+        String[] str={"bananna","apple","orange","mango"};
+        for(String fruits:str){
+            System.out.print(fruits+" ");
+        }
+    }
+}
