a698643857dcc0fa1823cbd157af586893c9e785
hemanthkorubilli <130943446+hemanthkorubilli@users.noreply.github.com>
2024-02-18 09:33:39 +0530

    Add files via upload

---
+++ strings.java
@@ -0,0 +1,9 @@
+class strings{
+    public static void main(String args[]){
+        String text="Hello";
+        String str="Raghu institute of technology";
+        String rem=text+" "+str;
+        System.out.println(rem);
+        System.out.print(text+" "+str);
+    }
+}
