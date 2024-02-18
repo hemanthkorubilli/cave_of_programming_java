9e57f6913dcf39d4829da731249b90a7aad7a4fd
hemanthkorubilli <130943446+hemanthkorubilli@users.noreply.github.com>
2024-02-18 10:36:29 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,10 @@
+class ifelse{
+    public static void main(String args[]){
+        int a=2,b=3;
+        if(a<b){
+            System.out.print("a is less than b");
+        }else{
+            System.out.print("a is greater than b");
+        }
+    }
+}
