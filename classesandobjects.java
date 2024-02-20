db01070008a1c3f4245e87b953207e4db7787305
hemanthkorubilli <130943446+hemanthkorubilli@users.noreply.github.com>
2024-02-20 06:17:35 +0530

    Add files via upload

---
+++ classesandobjects.java
@@ -0,0 +1,15 @@
+class Person{
+    String name;
+    int age;
+}
+public class classesandobjects{
+    public static void main(String args[]){
+        Person person1=new Person();
+        person1.name="Hemanth";
+        person1.age=22;
+        Person person2=new Person();
+        person2.name="Prakash";
+        person2.age=40;
+        System.out.print(person1.name);
+    }
+}
