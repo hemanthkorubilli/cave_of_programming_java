8acc3a6158e0dafabcd42bcfb646792d057bae16
hemanthkorubilli <130943446+hemanthkorubilli@users.noreply.github.com>
2024-02-23 08:28:48 +0530

    Add files via upload

---
+++ StaticKeyword.java
@@ -0,0 +1,28 @@
+package mainpakage;
+
+public class StaticKeyword {
+    public static void main(String[] args) {
+        Human one = new Human("hemanth", 22, 14000);
+        Human two = new Human("prakash", 40, 250000);
+        System.out.println("Name of first person:\n" + one.name);
+        System.out.println(one.population);
+        System.out.println("Name of second person:\n" + two.name);
+        System.out.println(two.population);
+
+    }
+}
+
+class Human {
+    String name;
+    int age;
+    int salary;
+    static int population;
+
+    Human(String name, int age, int salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+        Human.population += 1;
+
+    }
+}
