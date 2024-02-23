0edb94b0aa3efd31874efcdb3993cb61976f6a94
hemanthkorubilli <130943446+hemanthkorubilli@users.noreply.github.com>
2024-02-23 08:18:02 +0530

    Creating pakages

---
+++ Staticexample.java
@@ -0,0 +1,17 @@
+package demo.example;
+
+public class Staticexample {
+    int age;
+    String name;
+    int salary;
+    boolean married;
+
+    public Staticexample(int age, String name, int salary, boolean married) {
+        this.age = age;
+        this.name = name;
+        this.salary = salary;
+        this.married = married;
+
+    }
+
+}
---
+++ main.java
@@ -0,0 +1,9 @@
+package demo.example;
+
+public class main {
+    public static void main(String[] args) {
+        Staticexample member1 = new Staticexample(22, "hemanth", 10000, false);
+        System.out.print(member1.name);
+
+    }
+}
