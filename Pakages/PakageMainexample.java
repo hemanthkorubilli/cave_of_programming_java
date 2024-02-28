899c8a6358e06c07999deea1f24e92e57efd3791
hemanthkorubilli <130943446+hemanthkorubilli@users.noreply.github.com>
2024-02-28 07:11:18 +0530

    Add files via upload

---
+++ Pakages/PakageMainexample.java
@@ -0,0 +1,11 @@
+package Pakages;
+
+import static Pakages.Pakagemethod.message;
+
+public class PakageMainexample {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        message();
+
+    }
+}
