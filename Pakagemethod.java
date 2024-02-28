cfc72ab1f4d8f4aa542d67ecca436faa75c9f5a4
hemanthkorubilli <130943446+hemanthkorubilli@users.noreply.github.com>
2024-02-28 07:07:39 +0530

    Add files via upload

---
+++ Pakagemethod.java
@@ -0,0 +1,11 @@
+package Pakages;
+
+public class Pakagemethod {
+    public static void main(String[] args) {
+
+    }
+
+    public static void message() {
+        System.out.println("imported form another file method");
+    }
+}
