bb403c3d170e2a7b5b2659998e424064326e563c
hemanthkorubilli <130943446+hemanthkorubilli@users.noreply.github.com>
2024-03-10 14:18:12 +0530

    Add files via upload

---
+++ Sorting/SlectionSort.java
@@ -0,0 +1,33 @@
+package Sorting;
+
+import java.util.*;
+
+public class SlectionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int[] arr = new int[a];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        SelectionSorting(arr);
+    }
+
+    static void SelectionSorting(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
