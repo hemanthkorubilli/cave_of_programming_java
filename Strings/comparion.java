697909c9396f49d88a80b761af7e767f281e7c16
hemanthkorubilli <130943446+hemanthkorubilli@users.noreply.github.com>
2024-04-15 07:49:02 +0530

    Add files via upload

---
+++ Strings/comparion.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class comparion {
+    public static void main(String[] args) {
+        String s = "Hemanth";
+        String s1 = new String("Hemanth");
+        if (s.equals(s1)) {
+            System.out.println("yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
