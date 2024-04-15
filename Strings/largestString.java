982170346a38d3444c05c42122654c7cb6f187af
hemanthkorubilli <130943446+hemanthkorubilli@users.noreply.github.com>
2024-04-15 08:07:44 +0530

    Add files via upload

---
+++ Strings/largestString.java
@@ -0,0 +1,14 @@
+package Strings;
+
+public class largestString {
+    public static void main(String[] args) {
+        String[] str = { "apple", "bannana", "mango" };
+        String largest = str[0];
+        for (int i = 1; i < str.length; i++) {
+            if (largest.compareTo(str[i]) < 0) {
+                largest = str[i];
+            }
+        }
+        System.out.println(largest);
+    }
+}
