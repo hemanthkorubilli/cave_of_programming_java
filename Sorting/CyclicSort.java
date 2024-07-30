eadbe801dad08d22a7576c713adbe0a77dba33cb
hemanthkorubilli <130943446+hemanthkorubilli@users.noreply.github.com>
2024-07-30 15:36:26 +0530

    Add files via upload

---
+++ Sorting/CyclicSort.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class CyclicSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        cyclicSort(arr);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    static void cyclicSort(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
