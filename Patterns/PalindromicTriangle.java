1b1132f4ff3d81e93356f02d5cb3926cbe212dc4
hemanthkorubilli <130943446+hemanthkorubilli@users.noreply.github.com>
2024-08-20 08:42:50 +0530

    Add files via upload

---
+++ Patterns/PalindromicTriangle.java
@@ -0,0 +1,12 @@
+package Patterns;
+
+public class PalindromicTriangle {
+    public static void main(String[] args) {
+        int n=5;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= 2 * n; j++) {
+                if()
+            }
+        }
+    }
+}
